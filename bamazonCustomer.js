3bd424a8f9120ce31280c008670b254015037c60
Patrycja Gorczynska <patrycjagorczynska@Patrycjas-MacBook-Pro.local>
2019-02-03 22:23:18 -0700

    clear

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,7 +15,36 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-});
+
+connection.query('Select * from Products', function(err, res){
+        if(err) throw err;
+        start();
+
+        console.log('Welcome to BAMazon')
+  console.log('--------------------------------------')
+
+  for(var i = 0; i<res.length;i++){
+    console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+    console.log('--------------------------------------------------------------------------------------------------')
+  }
+
+    })
+
+function start(){
+    inquirer
+    .prompt([
+        {
+        name: "ID",
+        type: "input",
+        message: "What is the ID of the product you would like to buy?"
+        },
+        {
+            name: "Units",
+            type: "input",
+            message: "How many units of the product would you like to buy? "
+        }
+
+    ])
+}
+    
+
